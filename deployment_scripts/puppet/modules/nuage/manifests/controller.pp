98609e95fd2aa21ac36a823a5bc28c434a72e163
vpati011 <vishal.patil@nuagenetworks.net>
2015-09-13 16:02:55 -0400

    Fixed ordering

--- deployment_scripts/puppet/modules/nuage/manifests/controller.pp
+++ deployment_scripts/puppet/modules/nuage/manifests/controller.pp
@@ -14,15 +14,15 @@ class nuage::controller {
 
   file {'/etc/neutron/plugins/nuage/plugin.ini':
     content => template('nuage/plugin.ini.erb'),
+    require => Package['nuage-neutron'],
   } ->
   file {'/etc/neutron/plugin.ini':
     ensure => link,
     target  => '/etc/neutron/plugins/nuage/plugin.ini',
-    require => Package['nuage-neutron']
   }
 
   neutron_config {
     'DEFAULT/core_plugin':             value => 'neutron.plugins.nuage.plugin.NuagePlugin';
     'DEFAULT/service_plugins':         value => '';
-  } ->
+  }
 }
