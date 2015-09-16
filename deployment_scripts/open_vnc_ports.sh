34419b3c43155b13fa34eae8147b321402727d13
rparulek <rohan.s.parulekar@alcatel-lucent.com>
2015-09-15 19:14:16 -0700

    Removed shell script to open vnc ports

--- deployment_scripts/open_vnc_ports.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-lineno=$(iptables -nvL INPUT --line-numbers | grep "5900:6100" | awk '{print $1}')
-
-iptables -R INPUT $lineno -p tcp -s 0.0.0.0/0 -m multiport --dports 5900:6100 -j ACCEPT
-
-iptables-save > /etc/iptables/rules.v4
