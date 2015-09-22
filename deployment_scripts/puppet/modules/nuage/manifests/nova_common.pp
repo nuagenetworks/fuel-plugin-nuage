f9b7052f04ee6db3ebf4455cd694e02761121898
vpati011 <vishal.patil@nuagenetworks.net>
2015-09-21 22:18:48 -0400

    Changed nova parameters for nuage

--- deployment_scripts/puppet/modules/nuage/manifests/nova_common.pp
+++ deployment_scripts/puppet/modules/nuage/manifests/nova_common.pp
@@ -1,3 +1,5 @@
+# Nova parameters for Nuage
+
 class nuage::nova_common (
   $firewall_driver,
   $security_group_api,
@@ -7,44 +9,11 @@ class nuage::nova_common (
 ) {
 
   #Setting nova.conf parameters on all Openstack nodes
-
-  ini_setting { 'firewall_driver':
-  ensure  => present,
-  path    => '/etc/nova/nova.conf',
-  section => 'DEFAULT',
-  setting => 'firewall_driver',
-  value   => $firewall_driver,
-  }
-
-  ini_setting { 'security_group_api':
-    ensure  => present,
-    path    => '/etc/nova/nova.conf',
-    section => 'DEFAULT',
-    setting => 'security_group_api',
-    value   => $security_group_api,
-  }
-
-  ini_setting { 'libvirt_vif_driver':
-    ensure  => present,
-    path    => '/etc/nova/nova.conf',
-    section => 'DEFAULT',
-    setting => 'libvirt_vif_driver',
-    value   => $libvirt_vif_driver,
-  }
-
-  ini_setting { 'vif_driver':
-    ensure  => present,
-    path    => '/etc/nova/nova.conf',
-    section => 'DEFAULT',
-    setting => 'vif_driver',
-    value   => $vif_driver,
-  }
-
-  ini_setting { 'ovs_bridge':
-    ensure  => present,
-    path    => '/etc/nova/nova.conf',
-    section => 'neutron',
-    setting => 'ovs_bridge',
-    value   => $neutron_ovs_bridge,
+  nova_config {
+    'DEFAULT/firewall_driver': value => '$firewall_driver';
+    'DEFAULT/security_group_api': value => '$security_group_api';
+    'DEFAULT/libvirt_vif_driver' : value => '$libvirt_vif_driver';
+    'DEFAULT/vif_driver': value => '$vif_driver';
+    'neutron/ovs_bridge' : value => '$ovs_bridge';
   }
 }
