6d8a21aa642504500f8d0e755385d46b332cc7b3
rparulek <rohan.s.parulekar@alcatel-lucent.com>
2015-10-05 10:57:02 -0700

    Puppet changes to configure Nuage metadata agent service on the compute nodes

--- deployment_scripts/puppet/modules/nuage/manifests/init.pp
+++ deployment_scripts/puppet/modules/nuage/manifests/init.pp
@@ -4,7 +4,7 @@ class nuage {
 $settings = hiera('nuage')
 $nova_settings=hiera('nova')
 $neutron_settings=hiera('quantum_settings')
-$auth_ip=hiera('management_vip')
+$nova_auth_ip=hiera('management_vip')
 
 $net_partition_name = $settings['nuage_net_partition_name']
 $vsd_ip = $settings['nuage_vsd_ip']
@@ -17,7 +17,6 @@ $backup_controller =  $settings['nuage_backup_vsc_ip']
 
 ## Metadata settings
 $metadata_port = $settings['metadata_port']
-$nova_metadata_ip = $settings['nova_metadata_ip']
 $nova_metadata_port = $settings['nova_metadata_port']
 $nova_region_name = $settings['nova_region_name']
 $metadata_secret=$neutron_settings['metadata']['metadata_proxy_shared_secret']
