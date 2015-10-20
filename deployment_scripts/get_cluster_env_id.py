1c0275e0e7327a2cee3137bbb50f440469bd2f22
rparulek <rohan.s.parulekar@alcatel-lucent.com>
2015-10-20 15:04:35 -0700

    New change to iterate over network yaml files to compare mgmt vip from the primary controller to handle multiple cluster scenario

---
+++ deployment_scripts/get_cluster_env_id.py
@@ -0,0 +1,85 @@
+import argparse
+import paramiko
+import socket
+import logging
+import os
+import re
+import time
+import yaml
+import itertools
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+global host, username, password, copy_file
+
+parser = argparse.ArgumentParser(description="Copy the openstack upgrade package from Fuel")
+parser.add_argument('-i', '--host', help="IP address of the Fuel node",
+                    action='store')
+parser.add_argument('-u', '--username', help="User name for Fuel node login",
+                    action='store')
+parser.add_argument('-p', '--password', help="Password for Fuel node login",
+                    action='store')
+parser.add_argument('-v', '--vip', help="Management VIP for the cluster",
+                    action='store')
+
+args = parser.parse_args()
+host = args.host
+username = args.username
+password = args.password
+vip = args.vip
+
+def get_mos_cluster_id():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    file_list = list()
+    try:
+        ssh.connect(hostname=host, username=username, password=password, timeout=60.0)
+        stdin, stdout, stderr = ssh.exec_command("fuel env | grep 2 | awk '{print $1}'")
+        out = stdout.read()
+        if out is "":
+            logger.error("Could not obtain ENV ID")
+        else:
+            logger.info("Successfully obtained ENV ID")
+
+        file = open("/root/cluster_ids.txt", "w")
+        file.write(out)
+        file.close()
+
+        f = open("/root/cluster_ids.txt")
+        filecontents = f.readlines()
+        env_id_list = list()
+        for line in filecontents:
+            id = line.strip('\n')
+            env_id_list.append(id)
+
+        for id in env_id_list:
+            stdin, stdout, stderr = ssh.exec_command("fuel network --env %s --download" % id)
+            time.sleep(10)
+            file = "network_" + id + ".yaml"
+            file_list.append(file)
+
+        for file,id in itertools.izip(file_list,env_id_list):
+            stdin, stdout, stderr = ssh.exec_command("cat %s | grep management_vip | awk '{print $2}'" % file)
+            mgmt_vip = stdout.read()
+            if vip in mgmt_vip:
+                file = open("/root/mos_unique_id.txt", "w")
+                file.write(id)
+                file.close()
+                break
+
+        ssh.close()
+    except paramiko.AuthenticationException:
+        logger.error("Bad credentials for host")
+    except paramiko.SSHException:
+        logger.error("Issues with SSH service")
+    except socket.error:
+        logger.error("Host unreachable")
+
+def main():
+
+    get_mos_cluster_id()
+
+if __name__ == "__main__":
+    main()
--- deployment_scripts/puppet/modules/nuage/manifests/controller/get_mos_cluster_id.pp
+++ deployment_scripts/puppet/modules/nuage/manifests/controller/get_mos_cluster_id.pp
@@ -1,9 +1,9 @@
 class nuage::controller::get_mos_cluster_id {
 
   exec { 'get_cluster_env_id':
-    command  => "python /etc/fuel/plugins/nuage-1.0/get_mos_cluster_id.py \
+    command  => "python /etc/fuel/plugins/nuage-1.0/get_cluster_env_id.py \
 --host ${nuage::host} --username ${nuage::username} \
---password ${nuage::password}",
+--password ${nuage::password} --vip ${nuage::nova_auth_ip}",
     provider  => 'shell',
     path  => '/usr/bin:/usr/sbin:/bin:/sbin',
     environment  => "HOME=/root",
