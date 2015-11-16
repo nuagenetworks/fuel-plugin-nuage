4a0b02f311d0b9a00d39c3ebe2ec243cd9f37fb3
rparulek <rohan.s.parulekar@alcatel-lucent.com>
2015-11-15 18:24:20 -0800

    Fuel plugin code changes to incorporate Linux NIC bonding feature

--- deployment_scripts/pre_controller_setup.sh
+++ deployment_scripts/pre_controller_setup.sh
@@ -5,7 +5,7 @@ iptables -I INPUT $lineno -s 0.0.0.0/0 -p tcp -m multiport --dports 8775 -m comm
 
 iptables-save > /etc/iptables/rules.v4
 
-intf_list=$(ifconfig -a | grep "eth" | awk '{print $1}')
+intf_list=$(ifconfig -a | grep "bond" | awk '{print $1}')
 
 for intf in $intf_list
 do
